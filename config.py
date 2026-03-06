59eb654554805e6e748ec8d78ed6f86638d6ddfa
jankahmann-machtSachen <jan.kahmann@distart.de>
2026-03-06 14:52:15 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,34 @@
+"""
+Konfiguration für den Wikipedia Edit-War Scanner
+"""
+import os
+
+# Anzahl der Artikel pro automatischem Scan
+SCAN_LIMIT = 80
+
+# Zeitraum für Recent Changes (in Stunden)
+SCAN_HOURS = 48
+
+# Wikipedia API Endpoints
+WIKIS = {
+    'de': 'https://de.wikipedia.org/w/api.php',
+    'en': 'https://en.wikipedia.org/w/api.php'
+}
+
+# Gewichtung für Konflikt-Score (muss 1.0 ergeben)
+SCORE_WEIGHTS = {
+    'revert_ratio': 0.35,
+    'editor_conflict': 0.25,
+    'edit_frequency': 0.20,
+    'protection_level': 0.20
+}
+
+# Schwellenwerte
+MIN_EDITS_FOR_ANALYSIS = 5
+REVERT_KEYWORDS = ['revert', 'rv', 'undid', 'undo', 'zurückgesetzt', 'rückgängig']
+
+# Datenbank - Render nutzt /var/data für persistente Speicherung
+if os.environ.get('RENDER'):
+    DATABASE_PATH = '/var/data/editwars.db'
+else:
+    DATABASE_PATH = 'data/editwars.db'
